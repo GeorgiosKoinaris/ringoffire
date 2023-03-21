35bc8022af5c51b3383cf10c1c7bbe4a9256e494
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-03-21 11:01:11 +0100

    function for played cards added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,7 +8,8 @@ import { Game } from 'src/models/game';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  game?: Game;
+  currentCard: string;
+  game: Game;
 
   constructor() { }
 
@@ -21,6 +22,14 @@ export class GameComponent implements OnInit {
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
