d60cc56c02a3e714d25873e3401778c32498caa5
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-03-30 11:17:29 +0200

    added firebase to project

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'gkringoffire',
+    appId: '1:53857793002:web:5737d8092cfc4e96cff893',
+    storageBucket: 'gkringoffire.appspot.com',
+    apiKey: 'AIzaSyBtF1fPPeGNjDIFrL9-u1H1WQ6TW0C0fR4',
+    authDomain: 'gkringoffire.firebaseapp.com',
+    messagingSenderId: '53857793002',
+  },};
